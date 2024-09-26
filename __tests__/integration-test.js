6ed7454d06f6b875200108d250e4d796de729cc9
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-09-27 00:04:53 +0100

    added integration-test file with initial test for invalid paths

---
+++ __tests__/integration-test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const app = require("../app.js");
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed.js");
+const testData = require("../db/data/test-data/index.js");
+
+beforeEach(() => {
+  return seed(testData);
+});
+afterAll(() => db.end());
+
+describe("Invalid paths", () => {
+  test("status 404: returns 'Route not found' when path contains invalid path", () => {
+    return request(app)
+      .get("/api/invalid_path")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Route not found");
+      });
+  });
+});
