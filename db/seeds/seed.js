ba40e8aad8ee8b5037e5a0f419bcb669d299199e
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-09-26 23:43:29 +0100

    added seed.js

---
+++ db/seeds/seed.js
@@ -0,0 +1,55 @@
+const format = require("pg-format");
+const db = require("../connection");
+
+
+const seed = ({ topicData, userData, articleData, commentData }) => {
+  return db
+    .query(`DROP TABLE IF EXISTS bookings;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS tables;`);
+    })
+    .then(() => {
+      const bookingsTablePromise = db.query(`
+      CREATE TABLE bookings (
+        booking_id SERIAL PRIMARY KEY,
+        name VARCHAR NOT NULL,
+        number_of_guests NUMBER NOT NULL,
+        date DATE NOT NULL,
+        time TIME NOT NULL,
+        notes VARCHAR
+      );`);
+
+      const tablesTablePromise = db.query(`
+      CREATE TABLE table (
+      table_id SERIAL PRIMARY KEY,
+       capacity NUMBER NOT NULL,
+       notes VARCHAR
+
+       );`);
+
+      return Promise.all([bookingsTablePromise, tablesTablePromise]);
+    })
+    .then(() => {
+      const insertBookingsQueryStr = format(
+        "INSERT INTO bookings (name, number_of_guests, date, time, notes) VALUES %L;",
+        bookingData.map(({ name, number_of_guests, date, time, notes }) => [
+          name,
+          number_of_guests,
+          date,
+          time,
+          notes,
+        ])
+      );
+      const bookingsPromise = db.query(insertBookingsQueryStr);
+
+      const insertTablesQueryStr = format(
+        "INSERT INTO tables ( capacity, notes) VALUES %L;",
+        userData.map(({ capacity, notes }) => [capacity, notes])
+      );
+      const tablesPromise = db.query(insertTablesQueryStr);
+
+      return Promise.all([bookingsPromise, tablesPromise]);
+    })
+};
+
+module.exports = seed;
