a266ffc365490192fce7c0601be0b0c0e1b36526
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-09-26 23:59:58 +0100

    populated dev and test data files

---
+++ db/data/development-data/tables.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { capacity: 6, notes: "by window" },
+  {
+    capacity: 5,
+  },
+  {
+    capacity: 4,
+  },
+  { capacity: 6, notes: "outside" },
+];
---
+++ db/data/test-data/tables.js
@@ -0,0 +1,15 @@
+module.exports = [
+{capacity: 6,
+    notes: 'by window'
+},
+{
+    capacity: 5
+}, 
+{
+    capacity: 4
+}, 
+{capacity: 6, 
+    notes: 'outside'
+}
+
+]
