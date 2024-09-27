a896da50b58d9574411dce8c87df9f02a4dcfec5
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-09-27 01:17:40 +0100

    fixes errors in app.js

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 const express = require("express");
-
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.all("*", ( res) => {
+// Catch all invalid paths
+app.all("*", (req, res) => {
+  // Include 'req' here
   res.status(404).send({ msg: "Route not found" });
 });
 
-app.use((err,  res, next) => {
+// Custom error handler for known errors
+app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
-  } else next(err);
+  } else {
+    next(err); // Pass the error to the next error handler
+  }
 });
 
-app.use((err,  res, next) => {
+// Custom error handler for PostgreSQL error (e.g., invalid input)
+app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid input: expected a number" });
-  } else next(err);
+  } else {
+    next(err);
+  }
 });
 
-app.use((res) => {
-  res.status(500).send({ msg: "internal server error" });
+// General error handler for uncaught errors
+app.use((err, req, res, next) => {
+  console.error(err.stack); 
+  res.status(500).send({ msg: "Internal Server Error" });
 });
-
 
 module.exports = app;
