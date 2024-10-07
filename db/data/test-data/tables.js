6dfb327de191c10536c07b60fb613fa828d7bceb
Catherine Pulman <159820305+Clatherine@users.noreply.github.com>
2024-10-07 23:08:59 +0100

    updated data to remove restaurant ids

--- db/data/test-data/tables.js
+++ db/data/test-data/tables.js
@@ -1,15 +1,14 @@
 module.exports = [
-  { capacity: 6, notes: "by window", restaurant_id: 1 },
-  {capacity: 5,restaurant_id: 1},
-  {capacity: 4,restaurant_id: 1,},
-  { capacity: 6, notes: "outside", restaurant_id: 1 },
-  { capacity: 2, restaurant_id: 1 },
-  { capacity: 1, restaurant_id: 1 },
-  { capacity: 10,restaurant_id: 1,
+  { capacity: 6, notes: "by window" },
+  {capacity: 5,},
+  {capacity: 4,},
+  { capacity: 6, notes: "outside" },
+  { capacity: 2 },
+  { capacity: 1},
+  { capacity: 10
   },
-  // new restaurant
-  {capacity: 2, restaurant_id: 2 },
-    { capacity: 4, restaurant_id: 2 },
-    { capacity: 6, restaurant_id: 2 },
+  {capacity: 2 },
+    { capacity: 4 },
+    { capacity: 6},
 
 ];
